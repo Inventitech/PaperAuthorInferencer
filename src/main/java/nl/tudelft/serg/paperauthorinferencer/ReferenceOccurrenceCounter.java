c37b8a2c193b4f3ad2c6dd8af82b518d0e23f7f2
Moritz Beller <moritzbeller@gmx.de>
2015-07-21 15:37:35 +0200

    Adds feature to recognize multi-citation groups.

--- src/main/java/nl/tudelft/serg/paperauthorinferencer/ReferenceOccurrenceCounter.java
+++ src/main/java/nl/tudelft/serg/paperauthorinferencer/ReferenceOccurrenceCounter.java
@@ -1,6 +1,11 @@
 package nl.tudelft.serg.paperauthorinferencer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -16,6 +21,54 @@ public class ReferenceOccurrenceCounter {
 	public Set<Reference> references;
 
 	public void countReferences() {
+		countSimpleReferences();
+		countMultipleReferences();
+	}
+
+	private void countMultipleReferences() {
+		Pattern pattern = Pattern.compile("\\[(.*?)\\]");
+		Matcher matcher = pattern.matcher(paper.nonRefContent);
+		while (matcher.find()) {
+			String group = matcher.group(1);
+			if (group.contains(", ")) {
+				List<String> subRefs = Arrays.asList(group.split(", "));
+
+				addOccurrences(subRefs);
+			} else if (group.contains("-")) {
+				List<String> subRefs = new ArrayList <String> (Arrays
+						.asList(group.split("(-|–)")));
+
+				if (subRefs.size() != 2) {
+					continue;
+				}
+				int from = Integer.valueOf(subRefs.get(0));
+				int to = Integer.valueOf(subRefs.get(1));
+				for (int i = from + 1; i < to; i++) {
+					subRefs.add(String.valueOf(i));
+				}
+
+				addOccurrences(subRefs);
+			}
+		}
+	}
+
+	private void addOccurrences(List<String> subRefs) {
+		references.forEach(r -> {
+			String strippedIdentifier = stripIdentifier(r);
+			if (subRefs.contains(strippedIdentifier)) {
+				r.occurences += 1;
+			}
+		});
+	}
+
+	private String stripIdentifier(Reference r) {
+		String strippedIdentifier = r.identifier.replace("[", "");
+		strippedIdentifier = strippedIdentifier.replace("]", "");
+		return strippedIdentifier;
+
+	}
+
+	private void countSimpleReferences() {
 		references.forEach(r -> {
 			r.occurences = StringUtils
 					.countMatches(paper.content, r.identifier);
---
+++ src/test/java/nl/tudelft/serg/paperauthorinferencer/ReferenceOccurrenceCounterTest.java
@@ -0,0 +1,63 @@
+package nl.tudelft.serg.paperauthorinferencer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ReferenceOccurrenceCounterTest {
+
+	private static PDFPaper pdf;
+
+	@BeforeClass
+	public static void setupPDFPaper() {
+		pdf = new PDFPaper("src/test/resources/msr2014.pdf");
+
+	}
+
+	@Test
+	public void testSingleOccuringReferences() {
+		pdf.nonRefContent = "This is a multi-reference [1], [2], [3]. See how often it occurs [1]?";
+		pdf.content = pdf.nonRefContent;
+		int numberOfCountedReferences = countNumberOfOccuringReferences(buildReferences());
+		assertEquals(4, numberOfCountedReferences);
+	}
+
+	@Test
+	public void testMultipleOccuringReferences() {
+		pdf.nonRefContent = "This is a multi-reference [1, 2, 3]. See how often it occurs [1]?";
+		pdf.content = pdf.nonRefContent;
+		int numberOfCountedReferences = countNumberOfOccuringReferences(buildReferences());
+		assertEquals(4, numberOfCountedReferences);
+	}
+	
+	@Test
+	public void testRangeOccuringReferences() {
+		pdf.nonRefContent = "This is a multi-reference [1-3]. See how often it occurs [1]?";
+		pdf.content = pdf.nonRefContent;
+		int numberOfCountedReferences = countNumberOfOccuringReferences(buildReferences());
+		assertEquals(4, numberOfCountedReferences);
+	}
+
+	private Set<Reference> buildReferences() {
+		Set<Reference> references = new HashSet<Reference>();
+		references.add(new Reference("[1]"));
+		references.add(new Reference("[2]"));
+		references.add(new Reference("[3]"));
+		return references;
+	}
+
+	private int countNumberOfOccuringReferences(Set<Reference> references) {
+		ReferenceOccurrenceCounter referenceCounter = new ReferenceOccurrenceCounter(
+				pdf, references);
+		referenceCounter.countReferences();
+		return referenceCounter.references.stream().reduce((r, e) -> {
+			r.occurences += e.occurences;
+			return r;
+		}).get().occurences;
+	}
+
+}
